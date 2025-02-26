63a2218ccc169b5b5d5a7ae40d6794cc46efd1cf
ELiton Camargo <camargoliveira@gmail.com>
2025-02-26 10:33:25 -0300

    Criando estilo

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { Image } from 'expo-image';
 
 export default function App() {
   return (
     <View style={estilos.container}>
-        <Text style={estilos.meuApp}>
-          Meu App
-        </Text>
+      <Image 
+        source={{uri: 'https://img.elo7.com.br/product/685x685/39D9819/2-adesivo-bart-simpson-adesivo-bart-bart-de-lado-adesivos-motos.jpg'}}   
+        style={estilos.logo}
+      />
       <StatusBar style="auto" />
     </View>
   );
@@ -15,12 +17,20 @@ export default function App() {
 const estilos = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#8a8a8a',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
+    paddingTop: 20
   },
-  meuApp:{
-    fontSize: 28,
-    color: '#9b9bc776'
+  logo:{
+    width: 150, 
+    height: 150,
+    borderRadius: 75,
+    borderStyle: 'solid',
+    borderWidth: 4,
+    borderRightColor: '#000',
+    borderTopColor: '#290b0b',
+    borderLeftColor: '#9b0a0a',
+    borderBottomColor: '#490404',
   }
 });
