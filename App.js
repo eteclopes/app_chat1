b7935b47e26b31e5943165c71d54c1005c27bda7
ELiton Camargo <camargoliveira@gmail.com>
2025-03-10 10:33:20 -0300

    Trabalhando com bordas

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { Image } from 'expo-image';
+import ImgHomer from './assets/homer.png';
+
 
 export default function App() {
   return (
@@ -9,12 +11,31 @@ export default function App() {
         source={{uri: 'https://img.elo7.com.br/product/685x685/39D9819/2-adesivo-bart-simpson-adesivo-bart-bart-de-lado-adesivos-motos.jpg'}}   
         style={estilos.logo}
       />
+
+      <View style={estilos.cardImage}>
+        <Image
+          source={ImgHomer}
+          style={estilos.logo} 
+        />
+      </View>
+
+
       <StatusBar style="auto" />
     </View>
   );
 }
 
 const estilos = StyleSheet.create({
+  cardImage:{
+    backgroundColor: '#c22222',
+    marginTop: 20,
+    padding: 20,
+    borderStyle: 'solid',
+    borderWidth: 2, 
+    borderColor: '#000',
+    borderTopLeftRadius: '50%',
+    borderBottomRightRadius: '50%',
+  },
   container: {
     flex: 1,
     backgroundColor: '#8a8a8a',
