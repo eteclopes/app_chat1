577c1787159385190197b7994ab3ddeefc3416bc
ELiton Camargo <camargoliveira@gmail.com>
2025-03-12 10:40:50 -0300

    direção de componentes

--- App.js
+++ App.js
@@ -7,7 +7,7 @@ import ImgHomer from './assets/homer.png';
 export default function App() {
   return (
     <View style={estilos.container}>
-      <Image 
+      {/* <Image 
         source={{uri: 'https://img.elo7.com.br/product/685x685/39D9819/2-adesivo-bart-simpson-adesivo-bart-bart-de-lado-adesivos-motos.jpg'}}   
         style={estilos.logo}
       />
@@ -17,6 +17,36 @@ export default function App() {
           source={ImgHomer}
           style={estilos.logo} 
         />
+      </View> */}
+
+      <View style={estilos.view_p1}>
+          <View style={estilos.view_f}>
+              <Text style={estilos.letra}>A</Text>
+          </View>
+          <View style={estilos.view_f}>
+              <Text style={estilos.letra}>B</Text>
+          </View>
+          <View style={estilos.view_f}>
+              <Text style={estilos.letra}>R</Text>
+          </View>
+          <View style={estilos.view_f}>
+              <Text style={estilos.letra}>H</Text>
+          </View>
+      </View>
+
+      <View style={estilos.view_p2}>
+          <View style={estilos.view_f}>
+              <Text style={estilos.letra}>A</Text>
+          </View>
+          <View style={estilos.view_f}>
+              <Text style={estilos.letra}>B</Text>
+          </View>
+          <View style={estilos.view_f}>
+              <Text style={estilos.letra}>R</Text>
+          </View>
+          <View style={estilos.view_f}>
+              <Text style={estilos.letra}>H</Text>
+          </View>
       </View>
 
 
@@ -26,16 +56,6 @@ export default function App() {
 }
 
 const estilos = StyleSheet.create({
-  cardImage:{
-    backgroundColor: '#c22222',
-    marginTop: 20,
-    padding: 20,
-    borderStyle: 'solid',
-    borderWidth: 2, 
-    borderColor: '#000',
-    borderTopLeftRadius: '50%',
-    borderBottomRightRadius: '50%',
-  },
   container: {
     flex: 1,
     backgroundColor: '#8a8a8a',
@@ -43,15 +63,45 @@ const estilos = StyleSheet.create({
     justifyContent: 'flex-start',
     paddingTop: 20
   },
-  logo:{
-    width: 150, 
-    height: 150,
-    borderRadius: 75,
+  view_p2:{
+    flexDirection: 'row-reverse'
+  },
+  view_f:{
+    backgroundColor: '#FFF',
+    margin: 10,
+    width: 80,
+    height: 80,
     borderStyle: 'solid',
-    borderWidth: 4,
-    borderRightColor: '#000',
-    borderTopColor: '#290b0b',
-    borderLeftColor: '#9b0a0a',
-    borderBottomColor: '#490404',
+    borderWidth: 3,
+    borderColor: '#000',
+    borderRadius: '50%',
+    alignItems: 'center',
+    justifyContent:'center'
+  },
+  letra:{
+    fontSize: 30,
+    fontWeight: '500'
   }
+  // logo:{
+  //   backgroundColor: '#8a8a8a',
+  //   width: 150, 
+  //   height: 150,
+  //   borderRadius: 75,
+  //   borderStyle: 'solid',
+  //   borderWidth: 4,
+  //   borderRightColor: '#000',
+  //   borderTopColor: '#290b0b',
+  //   borderLeftColor: '#9b0a0a',
+  //   borderBottomColor: '#490404',
+  // },
+    // cardImage:{
+  //   backgroundColor: '#ffffff',
+  //   marginTop: 20,
+  //   padding: 20,
+  //   borderStyle: 'solid',
+  //   borderWidth: 2, 
+  //   borderColor: '#000',
+  //   borderTopLeftRadius: '50%',
+  //   borderBottomRightRadius: '50%',
+  // },
 });
